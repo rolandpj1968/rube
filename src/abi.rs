059957f9cd8fc2b5216cb4225e723a74f0d6d98f
Roland Paterson-Jones <rolandpj@gmail.com>
2024-04-27 23:48:20 +0200

    Removed manual bi loop from elimsb()

--- src/abi.rs
+++ src/abi.rs
@@ -1,4 +1,4 @@
-use crate::all::{isargbh, isparbh, isretbh, BlkIdx, Blks, Fn, J, O};
+use crate::all::{isargbh, isparbh, isretbh, Fn, J, O};
 
 /* eliminate sub-word abi op
  * variants for targets that
@@ -8,20 +8,16 @@ use crate::all::{isargbh, isparbh, isretbh, BlkIdx, Blks, Fn, J, O};
  */
 // TODO - f now does now need to be mut here (interior mutability)
 pub fn elimsb(f: &mut Fn) {
-    let blks: &Blks = &f.blks;
-
-    blks.for_each_bi(|bi| {
-        blks.with_mut(bi, |b| {
-            for i in b.ins_mut().iter_mut() {
-                if isargbh(i.op) {
-                    i.op = O::Oarg;
-                } else if isparbh(i.op) {
-                    i.op = O::Opar;
-                }
+    f.blks.for_each_mut(|b| {
+        for i in b.ins_mut().iter_mut() {
+            if isargbh(i.op) {
+                i.op = O::Oarg;
+            } else if isparbh(i.op) {
+                i.op = O::Opar;
             }
-            if isretbh(b.jmp().typ) {
-                b.jmp_mut().typ = J::Jretw;
-            }
-        });
+        }
+        if isretbh(b.jmp().typ) {
+            b.jmp_mut().typ = J::Jretw;
+        }
     });
 }
